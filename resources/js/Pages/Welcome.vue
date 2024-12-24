9fd8b3c524f70c439fe9da700c6b111ca318fc5f
Smae Tongmenglay <smaetongmenglay@MENGLAY.local>
2024-12-25 00:52:35 +0700

    started

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import { Button } from "@/Components/ui/button";
+import { Button } from "@/components/ui/button";
 </script>
 
 <template>
-    <div>
-        <Button>Click me</Button>
-    </div>
+    <Button>Button</Button>
 </template>
