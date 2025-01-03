711f194cb64b1231afe09de64b3bef1ac5be83cf
Menglay Smae <smaemenglay.coolbeans@hotmail.com>
2025-01-03 17:28:27 +0700

    .

--- database/migrations/2025_01_03_091403_create_teams_table.php
+++ database/migrations/2025_01_03_091403_create_teams_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('logo');
+            $table->string('logo')->nullable();
             $table->string('plan');
             $table->timestamps();
         });
